49a60afb325acc2b10fd575fc3e7bd66c048e2ea
theone0403 <125506576+theone0403@users.noreply.github.com>
2023-03-03 22:32:06 -0600

    Update final.py

--- final.py
+++ final.py
@@ -41,4 +41,93 @@ while 1:
         escape = {luck}""")
             break
 
-print("Are you ready to adventure?")
+input(f"\n{I}: Where am I, why is it so quiet here")
+input(f"\nenemey {I}: You’ve activated my trap card, lets fight!")
+input(f"\n{I}: Wait doppelganger was real? ?")
+input(f"\nenemy {I}: kahahahaha lets start the battle I going to take your body after beating you")
+input(f"\n{I}: ? ? ? !")
+
+enemyhealth = 50
+enemyarmor = 0
+enemyattack = 7
+enemyenergy = 0
+enemymental = 0
+print(f"enemy {I}'s health: {enemyhealth}")
+health1 = health
+
+while 1:
+    print(f"\n{I}'s turn! ! !")
+    time.sleep(1)
+    N = input("q)attack w)defend e)skill r)run\n")
+    if N == "q":
+        print(f"{I} attacked enemy")
+        attack1 = attack + random.choice([1, 2, 3, 5, 8])
+        if attack1 - enemyarmor <= 0:
+            print("enemy shield your attack")
+            enemyarmor -= attack1
+        else:
+            enemyhealth -= (attack1 - enemyarmor)
+            print(f"enemy's remain health = {enemyhealth}")
+            enemyarmor = 0
+    elif N == "w":
+        armor += 5 + mental
+        print(f'{I} prepared to shield the attack. Defense power became {armor}')
+    elif N == "e":
+        energy += 1
+        print("energy power~")
+        if energy ==2:
+            print(f"{I} throw energy wave")
+            enemyhealth -= mental + random.choice([10, 15, 20, 25, 30])
+            print(f"enemy's remain health = {enemyhealth}")
+            energy = 0
+    elif N == "r":
+        escape = random.random()
+        if escape < luck/10:
+            print(f"{I} successfully runaway from enemy but makes me feel awkward...")
+            escape += 1
+            break
+        else:
+            print("I failed to runaway! ! !")
+    if enemyhealth <= 0:
+        print(f"enemy {I}: damn.... I failed it again I will be back")
+        break
+        
+    N = random.randrange(1, 4)
+    print("\nenemey's turn! ! !")
+    time.sleep(1)
+    if N == 1:
+        enemyattack1 = enemyattack + random.choice([1, 2, 3, 5, 8])
+        if enemyattack1 - armor <= 0:
+            print("I sheild enemy's attack :)")
+            armor -= enemyattack1
+        else:
+            print("enemy striked suddenly")
+            health -= (enemyattack1 - armor)
+            print(f"{I}'s remain health = {health}")
+            armor = 0
+    elif N == 2:
+        enemyarmor += 5 + mental
+        print(f'enemy prepared to shield the attack. Defense power became {enemyarmor}')
+    elif N == 3:
+        enemyenergy += 1
+        print("energy power~")
+        if enemyenergy == 2:
+            print("enemy's energy wave!")
+            health -= enemymental + random.choice([10, 15, 20, 25, 30])
+            print(f"{I}'s remain helath = {health}")
+            enemyenergy = 0
+    if health <= 0:
+        print("I'm losing my conciousness")
+        print("\ntip: huh..... developer gonna laugh at you :(")
+        Count = input("Continue? q: retry w: retry e: retry.\n... ")
+        if Count != "if you got this your genious :)":
+            t = 4
+            while t >= 1:
+                t = t - 1
+                time.sleep(1)
+                print(f" loading {t}second")
+                health = health1
+                enemyhealth = 50
+                enemyarmor = 0
+                armor = 0
+                continue
